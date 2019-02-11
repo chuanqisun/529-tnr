e2321f463bfa7dc76f70f701849be582ade7e400
Chuanqi <henrysun918@gmail.com>
2019-02-10 18:39:50 -0800

    add github oauth flow

---
+++ sever/github-oauth2-server.webtask.js
@@ -0,0 +1,18 @@
+module.exports = async function(context, cb) {
+  const fetch = require('node-fetch');
+
+  const {code, state} = context.query;
+  const client_id = '93bc6d6286ff1f8d864c';
+  const client_secret = context.secrets.clientSecret;
+
+  const requestUrl = `https://github.com/login/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&code=${code}&state=${state}`;
+
+  const result = await fetch(requestUrl, {
+    headers: {
+      'Accept': 'application/json',
+    },
+  });
+  const resultJson = await result.json();
+
+  cb(null, resultJson);
+};
