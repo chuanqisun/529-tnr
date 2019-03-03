7a8c3720ad0da47a99f9e970e5ffc49660b5dcf7
Chuanqi <henrysun918@gmail.com>
2019-03-02 16:45:03 -0800

    add new rides to site

--- plan.md
+++ plan.md
@@ -3,6 +3,9 @@
 2. Build hourly from IFTTT: https://ifttt.com/applets/95767564d-deploy-529-rider-packet-site-hourly
 3. IFTTT uses webhook provided by Netlify to trigger build and deploy
 4. Weather data from Dark Sky: https://darksky.net/dev/account
+5. Ride emails collected from gmail with label "529tnr-bot" and stored in [a personal google drive folder](https://drive.google.com/drive/u/0/folders/1vEI6XEdd_8ryebe8ZZdOx4DHYNMe0dwd) with [a Flow app](https://us.flow.microsoft.com/manage/environments/1072bc6d-8e65-487f-a8e8-c6283578456a/flows/384f6125-9ef7-4a81-a88b-bf721116511a/details)
+6. The google drive folder is manually shared with a service account user.
+7. Ride announcements queried at build time with [Google API](https://console.developers.google.com/apis/dashboard?folder=&organizationId=&project=tnr-233319) authenticated as [the service account](https://console.developers.google.com/iam-admin/serviceaccounts/details/103768907512320484334?folder=&organizationId=&project=tnr-233319).
 
 # Roadmap
 1. This week's ride from email announcement
---
+++ src/data/query/announcements.js
@@ -0,0 +1,58 @@
+const EMAIL_FOLDER_ID = '1vEI6XEdd_8ryebe8ZZdOx4DHYNMe0dwd';
+const RECENCY_LIMIT = 10;
+
+const {google} = require('googleapis');
+
+
+
+module.exports = async () => {
+  let credentials = {};
+  if (process.env.GOOGLEAPI_CLIENT_EMAIL && process.env.GOOGLEAPI_PRIVATE_KEY) {
+    console.log('process.env.GOOGLEAPI_CLIENT_EMAIL and GOOGLEAPI_PRIVATE_KEY exists, query API');
+    credentials = {
+      client_email: process.env.GOOGLEAPI_CLIENT_EMAIL,
+      private_key: process.env.GOOGLEAPI_PRIVATE_KEY,
+    }
+  } else {
+    console.log('process.env.GOOGLEAPI_CLIENT_EMAIL and GOOGLEAPI_PRIVATE_KEY does not exists, use test-api-keys.json');
+    credentials = {
+      client_email: require('../../../test-api-keys.json').googleApi.client_email,
+      private_key: require('../../../test-api-keys.json').googleApi.private_key,
+    }
+  }
+
+  const auth = await google.auth.getClient({
+    credentials,
+    scopes: ['https://www.googleapis.com/auth/drive'],
+  });
+
+  await auth.authorize();
+
+  const drive = google.drive({
+    version: 'v3',
+    auth,
+  });
+
+  const content = await drive.files.list({
+    orderBy: 'name desc',
+    pageSize: RECENCY_LIMIT,
+    q: `'${EMAIL_FOLDER_ID}' in parents`,
+  });
+
+  const itemPromises = content.data.files.map(item => drive.files.get({
+    fileId: item.id,
+    alt: 'media',
+  }));
+
+  const items = await Promise.all(itemPromises);
+  const itemsData = items.map(item => {
+    // This interface is defined in the Flow
+    return {
+      body: item.data.body,
+      subject: item.data.subject,
+      time: new Date(item.data.time).toLocaleString("en-US", {month: 'short', day: 'numeric', timeZone: "America/Los_Angeles"}),
+    };
+  });
+
+  return itemsData;
+}
